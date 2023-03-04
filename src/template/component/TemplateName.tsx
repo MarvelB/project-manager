3ca9ea79d1d50f965ca3cd161f215f8b832c3f34
Bryan <bryan@devlift.io>
2023-03-04 10:35:12 -0500

    boilerplate is ready to start development

---
+++ src/template/component/TemplateName.tsx
@@ -0,0 +1,14 @@
+// @ts-ignore: Cannot find css module
+import styles from './TemplateName.module.css';
+
+interface TemplateNameProps {}
+
+const TemplateName = ({ }: TemplateNameProps) => {
+  return (
+    <div>
+      <h2>TemplateName</h2>
+    </div>
+  );
+}
+
+export default TemplateName;
