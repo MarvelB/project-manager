8a1293d613677d41a256fa2474afff2bec2c8bad
Bryan <bryan@devlift.io>
2023-03-09 15:29:28 -0500

    added firebase configurations files

---
+++ src/firebase/config.ts
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCHQMgSKBWhAAJAQwiMfFT07sHZaxvPU18",
+    authDomain: "bmaravilladev-projectmanager.firebaseapp.com",
+    projectId: "bmaravilladev-projectmanager",
+    storageBucket: "bmaravilladev-projectmanager.appspot.com",
+    messagingSenderId: "311329548477",
+    appId: "1:311329548477:web:443b00167b804fc6702fd4"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+
+const projectAuth = firebase.auth();
+
+const timeStamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timeStamp }
