ee03a505e27b15262bb843ba5282af2632e2bfd0
Bryan <bryan@devlift.io>
2023-03-09 17:47:30 -0500

    added initial code for signup. Need to hadle file input type

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,12 +1,60 @@
+import { useState } from 'react';
 import './Signup.css';
 
 interface SignupProps {}
 
 const Signup = ({ }: SignupProps) => {
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [displayName, setDisplayName] = useState<string>("");
+  const [image, setImage] = useState<null>(null);
+
   return (
-    <div>
-      <h2>Signup</h2>
-    </div>
+    <form className="auth-form">
+      <h2>Sign up</h2>
+
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          required
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          required
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+
+      <label>
+        <span>Display name:</span>
+        <input
+          type="text"
+          required
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+
+      <label>
+        <span>Profile name:</span>
+        <input
+          type="file"
+          required
+        />
+      </label>
+
+      <button className="btn">Sign up</button>
+
+    </form>
   );
 }
 
