7229654d0159dd80ece13587e50f83c3a89b0d0b
Bryan <bryan@devlift.io>
2023-03-13 17:08:46 -0400

    added route safeguards

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,62 @@
 import NavBar from 'components/NavBar/NavBar';
 import Sidebar from 'components/Sidebar/Sidebar';
+import { useAuthContext } from 'hooks/useAuthContext';
 import Create from 'pages/Create/Create';
 import Dashboard from 'pages/Dashboard/Dashboard';
 import Login from 'pages/Login/Login';
 import Project from 'pages/Project/Project';
 import Signup from 'pages/Signup/Signup';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
+
+  const { isAuthReady, user } = useAuthContext();
+
   return (
     <div className="App">
-      <BrowserRouter>
+      {isAuthReady && (
+        <BrowserRouter>
 
-        <Sidebar />
+          <Sidebar />
 
-        <div className="container">
+          <div className="container">
 
-          <NavBar />
+            <NavBar />
 
-          <Switch>
+            <Switch>
 
-            <Route path="/login">
-              <Login />
-            </Route>
+              <Route path="/login">
+                {!user && <Login />}
+                {user && <Redirect to="/" />}
+              </Route>
 
-            <Route path="/signup">
-              <Signup />
-            </Route>
+              <Route path="/signup">
+                {!user && <Signup />}
+                {user && <Redirect to="/" />}
+              </Route>
 
-            <Route path="/create">
-              <Create />
-            </Route>
+              <Route path="/create">
+                {user && <Create />}
+                {!user && <Redirect to="/login" />}
+              </Route>
 
-            <Route path="/projects/:id">
-              <Project />
-            </Route>
+              <Route path="/projects/:id">
+                {user && <Project />}
+                {!user && <Redirect to="/login" />}
+              </Route>
 
-            <Route path="/" exact>
-              <Dashboard />
-            </Route>
+              <Route path="/" exact>
+                {user && <Dashboard />}
+                {!user && <Redirect to="/login" />}
+              </Route>
 
-          </Switch>
+            </Switch>
 
-        </div>
+          </div>
 
-      </BrowserRouter>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
