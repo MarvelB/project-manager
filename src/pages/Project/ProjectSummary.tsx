453b585257b0e7958879b3cb2c1847a17856c70e
Bryan <bryan@devlift.io>
2023-03-15 18:53:23 -0400

    added functionality to delete projects

--- src/pages/Project/ProjectSummary.tsx
+++ src/pages/Project/ProjectSummary.tsx
@@ -1,4 +1,7 @@
 import Avatar from "components/Avatar/Avatar";
+import { useAuthContext } from "hooks/useAuthContext";
+import { useFirestore } from "hooks/useFirestore";
+import { useHistory } from "react-router-dom";
 import { ProjectModelWithId } from "types/project.model";
 
 interface ProjectSummaryProps {
@@ -6,10 +9,25 @@ interface ProjectSummaryProps {
 }
 
 const ProjectSummary = ({ project }: ProjectSummaryProps) => {
+
+    const { deleteDocument, response } = useFirestore<ProjectModelWithId>("projects");
+    const history = useHistory();
+
+    const { user } = useAuthContext();
+
+    const handleDelete = async (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+
+        deleteDocument(project.id);
+
+        history.push("/");
+    }
+
     return (
         <div>
             <div className="project-summary">
                 <h2 className="title">{project.name}</h2>
+                <p>By {project.createdBy.displayName}</p>
                 <p className="due-date">Project due by {project.dueDate.toDate().toDateString()}</p>
                 <p className="details">
                     {project.details}
@@ -23,6 +41,13 @@ const ProjectSummary = ({ project }: ProjectSummaryProps) => {
                     ))}
                 </div>
             </div>
+            {project.createdBy.id === user?.uid && (
+                <button
+                    className="btn"
+                    onClick={handleDelete}
+                    disabled={response.isLoading}
+                >{ response.isLoading ? "Submitting" : "Mark as complete" }</button>
+            )}
         </div>
     )
 }
