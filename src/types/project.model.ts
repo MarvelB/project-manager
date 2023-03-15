cf5fd40a2b0f2bf9c059c875a5736adce3903ce8
Bryan <bryan@devlift.io>
2023-03-15 17:01:55 -0400

    added model for project comments

--- src/types/project.model.ts
+++ src/types/project.model.ts
@@ -5,13 +5,21 @@ import firebase from "firebase/app";
 
 export interface ProjectModelUser extends Omit<UserModelWithId, "signedIn"> {}
 
+export interface ProjectCommentModel {
+    userDisplayName: string;
+    userPhotoURL: string;
+    body: string;
+    createdAt:  firebase.firestore.Timestamp;
+    id: number;
+}
+
 export interface ProjectModel {
     name: string;
     details: string;
     dueDate:  firebase.firestore.Timestamp;
     category: ProjectCategories;
     assignedUsers: ProjectModelUser[];
-    comments: string[];
+    comments: ProjectCommentModel[];
     createdBy: ProjectModelUser;
     createdAt:  firebase.firestore.Timestamp;
 }
