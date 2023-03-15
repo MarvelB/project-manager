4d444c8f76268125675af1cd0b7204545489b3fc
Bryan <bryan@devlift.io>
2023-03-15 15:59:45 -0400

    added hook to pull realtime data from a document

---
+++ src/hooks/useDocument.ts
@@ -0,0 +1,31 @@
+import { projectFirestore } from "firebase/config";
+import { useEffect, useState } from "react";
+
+export interface UseDocumentType<T> {
+    document: T | null;
+    error: string;
+}
+
+export const useDocument = <T>(collection: string, documentId: string): UseDocumentType<T> => {
+
+    const [document, setDocument] = useState<T | null>(null);
+    const [error, setError] = useState<string>("");
+
+    // get realtime data
+    useEffect(() => {
+        const docRef = projectFirestore.collection(collection).doc(documentId);
+
+        const unsubscribe = docRef.onSnapshot((snapshot) => {
+            setDocument({...snapshot.data, id: snapshot.id} as T);
+            setError("");
+        }, error => {
+            console.log(error);
+            setError(error.message);
+        });
+
+        return () => unsubscribe();
+
+    }, [collection, documentId]);
+
+    return {document, error}
+}
