74c5fb1879cc70d463930f5a33b92ec513e1bef6
Bryan <bryan@devlift.io>
2023-03-15 15:37:54 -0400

    finished styling the project list component

--- src/components/ProjectList/ProjectList.tsx
+++ src/components/ProjectList/ProjectList.tsx
@@ -1,3 +1,5 @@
+import Avatar from 'components/Avatar/Avatar';
+import { Link } from 'react-router-dom';
 import { ProjectModelWithId } from 'types/project.model';
 import './ProjectList.css';
 
@@ -7,10 +9,22 @@ interface ProjectListProps {
 
 const ProjectList = ({ projects }: ProjectListProps) => {
   return (
-    <div>
+    <div className="project-list">
       {projects.length === 0 && <p>No projects</p>}
       {projects.map(project => (
-        <div key={project.id}>{project.name}</div>
+        <Link key={project.id} to={`/projects/${project.id}`}>
+          <h4>{project.name}</h4>
+          <p>Due by {project.dueDate.toDate().toDateString()}</p>
+          <div className="assigned-to">
+            <ul>
+              {project.assignedUsers.map(user => (
+                <li key={user.photoURL}>
+                  <Avatar imageSrc={user.photoURL} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Link>
       ))}
     </div>
   );
