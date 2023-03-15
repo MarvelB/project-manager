a84d2899186300b48eaeeb935dc27b32974fbdd0
Bryan <bryan@devlift.io>
2023-03-15 15:25:46 -0400

    using new component in dashboard page

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,11 +1,21 @@
+import ProjectList from 'components/ProjectList/ProjectList';
+import { useCollection } from 'hooks/useCollection';
+import { ProjectModelWithId } from 'types/project.model';
 import './Dashboard.css';
 
 interface DashboardProps {}
 
 const Dashboard = ({ }: DashboardProps) => {
+
+  const { documents: projects, error } = useCollection<ProjectModelWithId>("projects");
+
   return (
     <div>
-      <h2>Dashboard</h2>
+      <h2 className="page-title">Dashboard</h2>
+
+      {error && <p className="error">{error}</p>}
+      {projects && <ProjectList projects={projects} />}
+      
     </div>
   );
 }
