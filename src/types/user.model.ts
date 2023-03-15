7922d6e652a7b729e7f1558dbfa14b8eebc92a73
Bryan <bryan@devlift.io>
2023-03-14 22:04:31 -0400

    added another model for users

--- src/types/user.model.ts
+++ src/types/user.model.ts
@@ -1,5 +1,9 @@
+import { WithID } from "./helper.model";
+
 export interface UserModel {
     photoURL: string;
     displayName: string;
     signedIn: boolean;
 }
+
+export interface UserModelWithId extends WithID<UserModel> {}
